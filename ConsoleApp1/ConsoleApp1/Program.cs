e20c10f8f23bc55c056710c629b97cc7e50b168a
Hege Áron <113238347+OneJackpot77@users.noreply.github.com>
2022-11-11 15:15:31 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleApp1;
+using ConsoleApp1.Properties;
+
+const int _indulokszama = 691;
+List<Versenyzo> versenyzo = new();
+
+//beolvasas
+
+using StreamReader sr = new(@"..\..\..\Src\bukkm2019.txt");
+_ = sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    versenyzo.Add(new Versenyzo(sr.ReadLine()));
+}
+
+Console.WriteLine("4.feladat" +
+    " Versenytávot ne teljesitettek aránya: " +
+    $"{100-versenyzo.Count / (double)_indulokszama * 100}%");
+
+int rttnvsz = versenyzo.Count(x => x.Tav == "Rövid" && !x.Nem);
+Console.WriteLine("5.feladat"+
+    " Női cersenyzők száma a rövidtavu versenyen:" +
+    $"{rttnvsz} fő");
+//6.feladat
+bool vetm6otv = versenyzo.Any(x => x.Ido.TotalHours > 6);
+Console.WriteLine("6.feladat"+
+    $"{(vetm6otv ? " Volt ilyen versenyzo" : "Nem volt ilyen versenyző")}");
+//7.feladat
+Console.WriteLine("7. feladat:" +
+    " A felnott ferfi kategoria gyoztese rovid tavon: ");
+Versenyzo rffgy = versenyzo
+    .Where(x => x.Kategoria == "ff" && x.Nem && x.Tav== "Rövid")
+    .OrderBy(x => x.Ido)
+    .First();
+
+Console.WriteLine(
+    $"\t Rajtszám: {rffgy.Rajtszam}\n" +
+    $"\t Név: {rffgy.Nev}\n" +
+    $"{(string.IsNullOrWhiteSpace(rffgy.Egyesulet)  ? null : $"\t Egyesulet: {rffgy.Egyesulet}")}\n" +
+    $"\t Idő: {rffgy.Ido}");
+//8.feladat:
+Console.WriteLine("8.feladat: " +
+    "Statisztika");
+var fks = versenyzo.Where(x => x.Nem)
+    .GroupBy(x => x.Kategoria);
+
+foreach (var item in fks)
+{
+    Console.WriteLine($"{item.Key} - {item.Count()} fő");
+}
---
+++ ConsoleApp1/ConsoleApp1/Versenyzo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Versenyzo
+    {
+        public string Rajtszam { get; set; }
+        public string Kategoria { get; set; }
+        public string Nev { get; set; }
+        public string? Egyesulet { get; set; }
+        public TimeSpan Ido { get; set; }
+
+        public string Tav => Rajtszam[0] switch
+        {
+            'M' => "Mini",
+            'R' => "Rövid",
+            'K' => "Közép",
+            'H' => "Hosszú",
+            'E' => "Pedelec",
+            _ => throw new Exception("hibás rajtszám!")
+        };
+
+        public bool Nem => Kategoria[Kategoria.Length - 1] == 'f';
+
+        public Versenyzo(string sor)
+        {
+            string[] v = sor.Split(';');
+            Rajtszam = v[0];
+            Kategoria = v[1];
+            Nev = v[2];
+            Egyesulet = v[3];
+            Ido = TimeSpan.Parse(v[4]);
+        }
+
+    }
+}
